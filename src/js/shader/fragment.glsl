0df999ca7cd72c179f78c43e6e063fad8e9eab95
yukaorange <takaokar19911013t@gmail.com>
2022-12-27 01:12:59 +0900

    shade progress sizeUp

--- src/js/shader/fragment.glsl
+++ src/js/shader/fragment.glsl
@@ -23,14 +23,14 @@ void main() {
 
   float sides = 2. * length(vUv.x - 0.5);
   float masking = step(0.9, sides);
-  float shade = 4. * (sides - 0.9) * masking;
+  float shade = 5. * (sides - 0.9) * masking;
   shade = shade * shade * shade * shade * shade;
   shade = pow(shade, 0.5);
 
-  newUV = (newUV - vec2(0.5, stepY - 0.1)) * (vec2(1. + 0.1 * shade, 1. + 0.7 * shade) - 0.6 * (1. - progress)) + vec2(0.5, stepY - 0.1);
+  newUV = (newUV - vec2(0.5, stepY - 0.1)) * (vec2(1. + 0.1 * shade, 1. + 0.7 * shade) - .4 * (1. - progress / 1.8)) + vec2(0.5, stepY - 0.1);
 
   float direction = (mod(ceil(newUV.y * 5.0), 2.) == 0.) ? -1. : 1.;
-  
+
   newUV.x = mod(newUV.x - scroll + progress * direction + hash1(stepY), 1.);
 
   newUV = fract(newUV * 5.);
